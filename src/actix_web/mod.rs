a5f3f9ca9b8e65c129dd0f9ec1ec58be1826e12e
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-07-10 12:14:55 +0200

    Add Generic Actix HandlerWrapper

--- src/actix_web/mod.rs
+++ src/actix_web/mod.rs
@@ -1,4 +1,4 @@
-use std::future::Future;
+use std::{future::Future, marker::PhantomData};
 
 use actix_web::{*, dev::Handler, web::{Data, Json}};
 use serde::{Serialize, de::DeserializeOwned};
@@ -23,7 +23,55 @@ impl <I: Serialize + DeserializeOwned + 'static, O: Serialize + DeserializeOwned
             },
         };
 
-        web::resource::<&str>(self.path.as_ref()).route(route.to(handler))
+        web::resource::<&str>(self.path.as_ref()).route(route.to(HandlerWrapper::new(handler)))
     }
     
 }
+
+pub struct HandlerWrapper<T, R, H> 
+where
+    H: Handler<T, R> + Clone + 'static,
+    R: Future,
+    R::Output: Responder, {
+
+        handler: H,
+        phantom_r: PhantomData<R>,
+        phantom_t: PhantomData<T>
+}
+
+impl <T, R, H> HandlerWrapper<T, R, H> 
+where
+    H: Handler<T, R> + Clone + 'static,
+    R: Future,
+    R::Output: Responder, {
+
+    pub fn new(handler: H) -> Self {
+        Self {
+            handler,
+            phantom_r: PhantomData,
+            phantom_t: PhantomData,
+        }
+    }
+}
+
+impl <T, R, H> Clone for HandlerWrapper<T, R, H>
+where
+    H: Handler<T, R> + Clone + 'static,
+    R: Future,
+    R::Output: Responder, {
+    fn clone(&self) -> Self {
+        Self::new(self.handler.clone())
+    }
+}
+
+impl <T: 'static, R: 'static, H: 'static> Handler<T, R> for HandlerWrapper<T, R, H> 
+where
+    H: Handler<T, R> + Clone + 'static,
+    R: Future,
+    R::Output: Responder,
+{
+    fn call(&self, param: T) -> R {
+        self.handler.call(param)
+    }
+}
+
