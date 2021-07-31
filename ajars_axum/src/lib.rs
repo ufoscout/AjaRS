791caf4c51ce4a56d2d2ca454ed55701a3a0fcde
Francesco Cina <mr.francesco.cina@gmail.com>
2021-07-31 23:05:22 +0200

    Partially working axum implementation

--- ajars_axum/src/lib.rs
+++ ajars_axum/src/lib.rs
@@ -20,7 +20,9 @@ where
 
     fn route<D, E, F, R>(&self, handler: F) -> BoxRoute<Body>
 where
-    F: 'static + Clone + Send + Sync + Fn(Request<Body>, Extension<D>, I) -> R,
+    F: 'static + Clone + Send + Sync + Fn(
+        //Request<Body>, 
+        Extension<D>, I) -> R,
     R: Future<Output = Result<O, E>> + Send,
     D: Clone + Send + Sync + 'static,
     E: IntoResponse + Send + 'static;
@@ -32,36 +34,46 @@ AxumRoute<I, O> for REST
 {
     fn route<D, E, F, R>(&self, handler: F) -> BoxRoute<Body>
 where
-    F: 'static + Clone + Send + Sync + Fn(Request<Body>, Extension<D>, I) -> R,
+    F: 'static + Clone + Send + Sync + Fn(
+        //Request<Body>, 
+        Extension<D>, I) -> R,
     R: Future<Output = Result<O, E>> + Send,
     D: Clone + Send + Sync + 'static,
     E: IntoResponse + Send + 'static
     {
 
         let route = match self.method() {
-            HttpMethod::DELETE => route(self.path(), delete(|req: Request<Body>, data: Extension<D>, payload: extract::Query<I>| async move {
-                let result = (handler)(req, data, payload.0).await;
+            HttpMethod::DELETE => route(self.path(), delete(|
+                //req: Request<Body>, 
+                data: Extension<D>, payload: extract::Query<I>| async move {
+                let result = (handler)(data, payload.0).await;
                 match result {
                     Ok(result) => Ok(response::Json(result)),
                     Err(e) => Err(e)
                 }
             })).boxed(),
-            HttpMethod::GET => route(self.path(), get(|req: Request<Body>, data: Extension<D>, payload: extract::Query<I>| async move {
-                let result = (handler)(req, data, payload.0).await;
+            HttpMethod::GET => route(self.path(), get(|
+                //req: Request<Body>, 
+                data: Extension<D>, payload: extract::Query<I>| async move {
+                let result = (handler)(data, payload.0).await;
                 match result {
                     Ok(result) => Ok(response::Json(result)),
                     Err(e) => Err(e)
                 }
             })).boxed(),
-            HttpMethod::POST => route(self.path(), post(|req: Request<Body>, data: Extension<D>, payload: extract::Json<I>| async move {
-                let result = (handler)(req, data, payload.0).await;
+            HttpMethod::POST => route(self.path(), post(|
+                //req: Request<Body>, 
+                data: Extension<D>, payload: extract::Json<I>| async move {
+                let result = (handler)(data, payload.0).await;
                 match result {
                     Ok(result) => Ok(response::Json(result)),
                     Err(e) => Err(e)
                 }
             })).boxed(),
-            HttpMethod::PUT => route(self.path(), put(|req: Request<Body>, data: Extension<D>, payload: extract::Json<I>| async move {
-                let result = (handler)(req, data, payload.0).await;
+            HttpMethod::PUT => route(self.path(), put(|
+                //req: Request<Body>, 
+                data: Extension<D>, payload: extract::Json<I>| async move {
+                let result = (handler)(data, payload.0).await;
                 match result {
                     Ok(result) => Ok(response::Json(result)),
                     Err(e) => Err(e)
@@ -73,8 +85,6 @@ where
     }
 }
 
-
-
 #[cfg(test)]
 mod tests {
 
@@ -82,7 +92,7 @@ mod tests {
 
     use super::*;
     use ajars_core::RestFluent;
-    use ::axum::{AddExtensionLayer, extract::Json};
+    use ::axum::AddExtensionLayer;
     use http::StatusCode;
     use serde::{Deserialize, Serialize};
     use tower::ServiceExt; // for `app.oneshot()`
@@ -97,7 +107,9 @@ mod tests {
         pub message: String,
     }
 
-    async fn ping(_request: Request<Body>, _data: Extension<()>, body: PingRequest) -> Result<PingResponse, ServerError> {
+    async fn ping(
+        //_request: Request<Body>, 
+        _data: Extension<()>, body: PingRequest) -> Result<PingResponse, ServerError> {
         Ok(PingResponse { message: body.message })
     }
 
@@ -118,7 +130,7 @@ mod tests {
 
     #[tokio::test]
     async fn should_create_a_delete_endpoint() {
- 
+
         // Arrange
         let rest = RestFluent::<PingRequest, PingResponse>::delete(format!(
             "/api/something/{}",
@@ -129,6 +141,7 @@ mod tests {
         let app = rest
             .route(ping)
             .layer(AddExtensionLayer::new(()));
+
 
         let payload = PingRequest {
             message: format!("message{}", rand::random::<usize>())
@@ -154,8 +167,7 @@ mod tests {
         assert_eq!(body.message, payload.message);
     }
 
-    /*
-    #[actix_rt::test]
+    #[tokio::test]
     async fn should_create_a_get_endpoint() {
  
         // Arrange
@@ -165,32 +177,38 @@ mod tests {
         ));
 
 
-        let mut app = test::init_service(
-            App::new()
-                .app_data(Data::new(()))
-                .service(rest.handle(ping)),
-        )
-        .await;
+        let app = rest
+            .route(ping)
+            .layer(AddExtensionLayer::new(()));
+
 
         let payload = PingRequest {
             message: format!("message{}", rand::random::<usize>())
         };
 
-        let req = test::TestRequest::get()
-        .uri(&format!("{}?message={}", rest.path(), payload.message))
-        .to_request();
-
         // Act
-        let resp: PingResponse = test::read_response_json(&mut app, req).await;
+        let response = app
+            .oneshot(Request::builder()
+                .method(http::Method::GET)
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .uri(&format!("{}?message={}", rest.path(), payload.message))
+                .body(Body::empty()).unwrap())
+            .await
+            .unwrap();
 
         // Assert
-        assert_eq!(resp.message, payload.message);
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: PingResponse = serde_json::from_slice(&body).unwrap();
+
+        // Assert
+        assert_eq!(body.message, payload.message);
     }
-    */
 
     #[tokio::test]
     async fn should_create_a_post_endpoint() {
- 
+
         // Arrange
         let rest = RestFluent::<PingRequest, PingResponse>::post(format!(
             "/api/something/{}",
@@ -228,8 +246,8 @@ mod tests {
         assert_eq!(body.message, payload.message);
     }
 
-    /*
-    #[actix_rt::test]
+
+    #[tokio::test]
     async fn should_create_a_put_endpoint() {
  
         // Arrange
@@ -238,26 +256,61 @@ mod tests {
             rand::random::<usize>()
         ));
 
-
-        let mut app = test::init_service(
-            App::new()
-                .app_data(Data::new(()))
-                .service(rest.handle(ping)),
-        )
-        .await;
+        let app = rest
+            .route(ping)
+            .layer(AddExtensionLayer::new(()));
 
         let payload = PingRequest {
             message: format!("message{}", rand::random::<usize>())
         };
 
-        let req = test::TestRequest::put().uri(rest.path()).set_json(&payload).to_request();
-
         // Act
-        let resp: PingResponse = test::read_response_json(&mut app, req).await;
+        let response = app
+            .oneshot(Request::builder()
+                .method(http::Method::PUT)
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .uri(rest.path())
+                .body(Body::from(
+                    serde_json::to_vec(&payload).unwrap(),
+                )).unwrap())
+            .await
+            .unwrap();
 
         // Assert
-        assert_eq!(resp.message, payload.message);
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: PingResponse = serde_json::from_slice(&body).unwrap();
+
+        // Assert
+        assert_eq!(body.message, payload.message);
     }
-*/
+
+
+
+#[tokio::test]
+async fn test_extractors() {
+
+    let app = route("/api", 
+    post(|req: Request<Body>, payload: extract::Json<String>| async move {
+        println!("CALL RECEIVED");
+    }));
+
+    // Act
+    let response = app
+        .oneshot(Request::builder()
+            .method(http::Method::POST)
+            .header(http::header::CONTENT_TYPE, "application/json")
+            .uri("/api")
+            .body(Body::from(
+                serde_json::to_vec(&"payload").unwrap(),
+            )).unwrap())
+        .await
+        .unwrap();
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+
+}
 
 }
