a004909ce814a7c850a65fbf580a9df5518b067e
Francesco Cina <mr.francesco.cina@gmail.com>
2021-07-31 20:38:18 +0200

    Add breaking tests

--- ajars_axum/src/lib.rs
+++ ajars_axum/src/lib.rs
@@ -74,162 +74,7 @@ where
 }
 
 
-/*
-pub trait Handler<D, R, E, I, O>: Clone + 'static
-where
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (RequestParts<()>, Extension<D>, I)) -> R;
-}
 
-impl<F, D, R, E, I, O> Handler<D, R, E, I, O> for F
-where
-    F: 'static + Clone + Fn(RequestParts<()>, Extension<D>, I) -> R,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (RequestParts<()>, Extension<D>, I)) -> R {
-        self(param.0, param.1, param.2)
-    }
-}
-
-pub struct JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    handler: H,
-    phantom_d: PhantomData<D>,
-    phantom_r: PhantomData<R>,
-    phantom_e: PhantomData<E>,
-    phantom_i: PhantomData<I>,
-    phantom_o: PhantomData<O>,
-}
-
-impl<H, D, R, E, I, O> JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    pub fn new(handler: H) -> Self {
-        Self {
-            handler,
-            phantom_d: PhantomData,
-            phantom_r: PhantomData,
-            phantom_e: PhantomData,
-            phantom_i: PhantomData,
-            phantom_o: PhantomData,
-        }
-    }
-}
-
-impl<H, D, R, E, I, O> Clone for JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn clone(&self) -> Self {
-        Self::new(self.handler.clone())
-    }
-}
-
-impl<H, D, R, E, I, O> AxumHandler<(RequestParts<()>, Extension<D>, extract::Json<I>), R> for JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    D: 'static,
-    R: Future<Output = Result<response::Json<O>, E>> + 'static,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    
-    type Sealed = Type;
-
-    fn call(&self, param: (RequestParts<()>, Extension<D>, extract::Json<I>)) -> R {
-        self.handler.call((param.0, param.1, param.2.into_inner()))
-    }
-}
-
-pub struct QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    handler: H,
-    phantom_d: PhantomData<D>,
-    phantom_r: PhantomData<R>,
-    phantom_e: PhantomData<E>,
-    phantom_i: PhantomData<I>,
-    phantom_o: PhantomData<O>,
-}
-
-impl<H, D, R, E, I, O> QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    pub fn new(handler: H) -> Self {
-        Self {
-            handler,
-            phantom_d: PhantomData,
-            phantom_r: PhantomData,
-            phantom_e: PhantomData,
-            phantom_i: PhantomData,
-            phantom_o: PhantomData,
-        }
-    }
-}
-
-impl<H, D, R, E, I, O> Clone for QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<response::Json<O>, E>>,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn clone(&self) -> Self {
-        Self::new(self.handler.clone())
-    }
-}
-
-impl<H, D, R, E, I, O> AxumHandler<(RequestParts<()>, Extension<D>, extract::Query<I>), R> for QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    D: 'static,
-    R: Future<Output = Result<response::Json<O>, E>> + 'static,
-    E: IntoResponse + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (RequestParts<()>, Extension<D>, extract::Query<I>)) -> R {
-        self.handler.call((param.0, param.1, param.2.into_inner()))
-    }
-}
-*/
-
-
-/*
 #[cfg(test)]
 mod tests {
 
@@ -237,8 +82,11 @@ mod tests {
 
     use super::*;
     use ajars_core::RestFluent;
+    use ::axum::{AddExtensionLayer, extract::Json};
+    use http::StatusCode;
     use serde::{Deserialize, Serialize};
-    
+    use tower::ServiceExt; // for `app.oneshot()`
+
     #[derive(Serialize, Deserialize, Debug)]
     pub struct PingRequest {
         pub message: String,
@@ -249,8 +97,8 @@ mod tests {
         pub message: String,
     }
 
-    async fn ping(_request: HttpRequest, _data: Data<()>, body: PingRequest) -> Result<Json<PingResponse>, ServerError> {
-        Ok(Json(PingResponse { message: body.message }))
+    async fn ping(_request: Request<Body>, _data: Extension<()>, body: PingRequest) -> Result<PingResponse, ServerError> {
+        Ok(PingResponse { message: body.message })
     }
 
     #[derive(Debug, Clone)]
@@ -262,9 +110,13 @@ mod tests {
         }
     }
 
-    impl ResponseError for ServerError {}
+    impl IntoResponse for ServerError {
+        fn into_response(self) -> http::Response<Body> {
+            "error".into_response()
+        }
+    }
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn should_create_a_delete_endpoint() {
  
         // Arrange
@@ -274,28 +126,35 @@ mod tests {
         ));
 
 
-        let mut app = test::init_service(
-            App::new()
-                .app_data(Data::new(()))
-                .service(rest.handle(ping)),
-        )
-        .await;
+        let app = rest
+            .route(ping)
+            .layer(AddExtensionLayer::new(()));
 
         let payload = PingRequest {
             message: format!("message{}", rand::random::<usize>())
         };
 
-        let req = test::TestRequest::delete()
-        .uri(&format!("{}?message={}", rest.path(), payload.message))
-        .to_request();
-
         // Act
-        let resp: PingResponse = test::read_response_json(&mut app, req).await;
+        let response = app
+            .oneshot(Request::builder()
+                .method(http::Method::DELETE)
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .uri(&format!("{}?message={}", rest.path(), payload.message))
+                .body(Body::empty()).unwrap())
+            .await
+            .unwrap();
 
         // Assert
-        assert_eq!(resp.message, payload.message);
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: PingResponse = serde_json::from_slice(&body).unwrap();
+
+        // Assert
+        assert_eq!(body.message, payload.message);
     }
 
+    /*
     #[actix_rt::test]
     async fn should_create_a_get_endpoint() {
  
@@ -327,8 +186,9 @@ mod tests {
         // Assert
         assert_eq!(resp.message, payload.message);
     }
+    */
 
-    #[actix_rt::test]
+    #[tokio::test]
     async fn should_create_a_post_endpoint() {
  
         // Arrange
@@ -338,26 +198,37 @@ mod tests {
         ));
 
 
-        let mut app = test::init_service(
-            App::new()
-                .app_data(Data::new(()))
-                .service(rest.handle(ping)),
-        )
-        .await;
+        let app = rest
+            .route(ping)
+            .layer(AddExtensionLayer::new(()));
 
         let payload = PingRequest {
             message: format!("message{}", rand::random::<usize>())
         };
 
-        let req = test::TestRequest::post().uri(rest.path()).set_json(&payload).to_request();
-
         // Act
-        let resp: PingResponse = test::read_response_json(&mut app, req).await;
+        let response = app
+            .oneshot(Request::builder()
+                .method(http::Method::POST)
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .uri(rest.path())
+                .body(Body::from(
+                    serde_json::to_vec(&payload).unwrap(),
+                )).unwrap())
+            .await
+            .unwrap();
 
         // Assert
-        assert_eq!(resp.message, payload.message);
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: PingResponse = serde_json::from_slice(&body).unwrap();
+
+        // Assert
+        assert_eq!(body.message, payload.message);
     }
 
+    /*
     #[actix_rt::test]
     async fn should_create_a_put_endpoint() {
  
@@ -387,6 +258,6 @@ mod tests {
         // Assert
         assert_eq!(resp.message, payload.message);
     }
+*/
 
 }
-*/
