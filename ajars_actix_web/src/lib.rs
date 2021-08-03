2e1f5094f959401b0fcc0c66172dda791271aac7
Francesco Cina <mr.francesco.cina@gmail.com>
2021-08-03 17:59:30 +0200

    Reimplement ActixWebHandler

--- ajars_actix_web/src/lib.rs
+++ ajars_actix_web/src/lib.rs
@@ -1,10 +1,6 @@
-use std::{future::Future, marker::PhantomData};
+use std::future::Future;
 
-use crate::actix_web::{
-    dev::Handler as ActixHandler,
-    web::{Data, Json, Query},
-    *,
-};
+use ::actix_web::{FromRequest, Resource, ResponseError, web::{self, Json}};
 use ajars_core::{HttpMethod, RestType};
 use serde::{de::DeserializeOwned, Serialize};
 
@@ -14,182 +10,72 @@ pub mod actix_web {
 
 mod attempt;
 
-pub trait HandleActix<I: Serialize + DeserializeOwned + 'static, O: Serialize + DeserializeOwned + 'static> {
-    fn handle<H, D, R, E>(&self, handler: H) -> Resource
-    where
-        H: Handler<D, R, E, I, O>,
-        D: 'static,
-        R: Future<Output = Result<Json<O>, E>> + 'static,
-        E: ResponseError + 'static;
+pub trait ActixWebHandler<I: Serialize + DeserializeOwned, O: Serialize + DeserializeOwned, T, H> {
+    fn handle(&self, handler: H) -> Resource;
 }
 
-impl<I: Serialize + DeserializeOwned + 'static, O: Serialize + DeserializeOwned + 'static, REST: RestType<I, O>>
-    HandleActix<I, O> for REST
+
+impl <I: Serialize + DeserializeOwned + 'static, O: Serialize + DeserializeOwned + 'static, H, R, E, REST: RestType<I, O>> ActixWebHandler<I, O, (), H>
+    for REST 
+where 
+H: Clone + 'static + Fn(I) -> R,
+R: Future<Output = Result<Json<O>, E>> + 'static,
+E: ResponseError + 'static,
 {
-    fn handle<H, D, R, E>(&self, handler: H) -> Resource
-    where
-        H: Handler<D, R, E, I, O>,
-        D: 'static,
-        R: Future<Output = Result<Json<O>, E>> + 'static,
-        E: ResponseError + 'static,
-    {
+    fn handle(&self, handler: H) -> Resource {
         let resource = web::resource::<&str>(self.path());
 
         match self.method() {
-            HttpMethod::DELETE => resource.route(web::delete().to(QueryHandlerWrapper::new(handler))),
-            HttpMethod::GET => resource.route(web::get().to(QueryHandlerWrapper::new(handler))),
-            HttpMethod::POST => resource.route(web::post().to(JsonHandlerWrapper::new(handler))),
-            HttpMethod::PUT => resource.route(web::put().to(JsonHandlerWrapper::new(handler))),
+            HttpMethod::DELETE => resource.route(web::delete().to(
+                move |json: Json<I>| {
+                (handler)(json.into_inner())
+            })),
+            HttpMethod::GET => resource.route(web::get().to(
+                move |json: Json<I>| {
+                (handler)(json.into_inner())
+            })),
+            HttpMethod::POST => resource.route(web::post().to(
+                move |json: Json<I>| {
+                (handler)(json.into_inner())
+            })),
+            HttpMethod::PUT => resource.route(web::put().to(
+                move |json: Json<I>| {
+                (handler)(json.into_inner())
+            })),
         }
     }
 }
 
-pub trait Handler<D, R, E, I, O>: Clone + 'static
-where
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (HttpRequest, Data<D>, I)) -> R;
-}
 
-impl<F, D, R, E, I, O> Handler<D, R, E, I, O> for F
-where
-    F: 'static + Clone + Fn(HttpRequest, Data<D>, I) -> R,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
+impl <I: Serialize + DeserializeOwned + 'static, O: Serialize + DeserializeOwned + 'static, H, R, E, P0, REST: RestType<I, O>> ActixWebHandler<I, O, (P0,), H>
+    for REST 
+where 
+H: Clone + 'static + Fn(I, P0) -> R,
+R: Future<Output = Result<Json<O>, E>> + 'static,
+E: ResponseError + 'static,
+P0: FromRequest + 'static
 {
-    fn call(&self, param: (HttpRequest, Data<D>, I)) -> R {
-        self(param.0, param.1, param.2)
-    }
-}
+    fn handle(&self, handler: H) -> Resource {
+        let resource = web::resource::<&str>(self.path());
 
-pub struct JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    handler: H,
-    phantom_d: PhantomData<D>,
-    phantom_r: PhantomData<R>,
-    phantom_e: PhantomData<E>,
-    phantom_i: PhantomData<I>,
-    phantom_o: PhantomData<O>,
-}
-
-impl<H, D, R, E, I, O> JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    pub fn new(handler: H) -> Self {
-        Self {
-            handler,
-            phantom_d: PhantomData,
-            phantom_r: PhantomData,
-            phantom_e: PhantomData,
-            phantom_i: PhantomData,
-            phantom_o: PhantomData,
+        match self.method() {
+            HttpMethod::DELETE => resource.route(web::delete().to(
+                move |json: Json<I>, p0: P0| {
+                (handler)(json.into_inner(), p0)
+            })),
+            HttpMethod::GET => resource.route(web::get().to(
+                move |json: Json<I>, p0: P0| {
+                (handler)(json.into_inner(), p0)
+            })),
+            HttpMethod::POST => resource.route(web::post().to(
+                move |json: Json<I>, p0: P0| {
+                (handler)(json.into_inner(), p0)
+            })),
+            HttpMethod::PUT => resource.route(web::put().to(
+                move |json: Json<I>, p0: P0| {
+                (handler)(json.into_inner(), p0)
+            })),
         }
-    }
-}
-
-impl<H, D, R, E, I, O> Clone for JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn clone(&self) -> Self {
-        Self::new(self.handler.clone())
-    }
-}
-
-impl<H, D, R, E, I, O> ActixHandler<(HttpRequest, Data<D>, Json<I>), R> for JsonHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    D: 'static,
-    R: Future<Output = Result<Json<O>, E>> + 'static,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (HttpRequest, Data<D>, Json<I>)) -> R {
-        self.handler.call((param.0, param.1, param.2.into_inner()))
-    }
-}
-
-pub struct QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    handler: H,
-    phantom_d: PhantomData<D>,
-    phantom_r: PhantomData<R>,
-    phantom_e: PhantomData<E>,
-    phantom_i: PhantomData<I>,
-    phantom_o: PhantomData<O>,
-}
-
-impl<H, D, R, E, I, O> QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    pub fn new(handler: H) -> Self {
-        Self {
-            handler,
-            phantom_d: PhantomData,
-            phantom_r: PhantomData,
-            phantom_e: PhantomData,
-            phantom_i: PhantomData,
-            phantom_o: PhantomData,
-        }
-    }
-}
-
-impl<H, D, R, E, I, O> Clone for QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    R: Future<Output = Result<Json<O>, E>>,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn clone(&self) -> Self {
-        Self::new(self.handler.clone())
-    }
-}
-
-impl<H, D, R, E, I, O> ActixHandler<(HttpRequest, Data<D>, Query<I>), R> for QueryHandlerWrapper<H, D, R, E, I, O>
-where
-    H: Handler<D, R, E, I, O> + Clone + 'static,
-    D: 'static,
-    R: Future<Output = Result<Json<O>, E>> + 'static,
-    E: ResponseError + 'static,
-    I: Serialize + DeserializeOwned + 'static,
-    O: Serialize + DeserializeOwned + 'static,
-{
-    fn call(&self, param: (HttpRequest, Data<D>, Query<I>)) -> R {
-        self.handler.call((param.0, param.1, param.2.into_inner()))
     }
 }
 
@@ -200,6 +86,7 @@ mod tests {
 
     use super::*;
     use crate::actix_web::test;
+    use ::actix_web::{App, HttpRequest, web::Data};
     use ajars_core::RestFluent;
     use serde::{Deserialize, Serialize};
     
@@ -213,7 +100,7 @@ mod tests {
         pub message: String,
     }
 
-    async fn ping(_request: HttpRequest, _data: Data<()>, body: PingRequest) -> Result<Json<PingResponse>, ServerError> {
+    async fn ping(body: PingRequest, _request: HttpRequest ) -> Result<Json<PingResponse>, ServerError> {
         Ok(Json(PingResponse { message: body.message }))
     }
 
