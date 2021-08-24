3975dd1289d79aee423f0ac35004f0a0a5fba3ae
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-08-24 16:41:33 +0200

    Add axum feature to ajars

--- ajars/src/lib.rs
+++ ajars/src/lib.rs
@@ -3,6 +3,11 @@ pub mod actix_web {
     pub use ajars_actix_web::*;
 }
 
+#[cfg(feature = "axum")]
+pub mod axum {
+    pub use ajars_axum::*;
+}
+
 #[cfg(feature = "reqwest")]
 pub mod reqwest {
     pub use ajars_reqwest::*;
