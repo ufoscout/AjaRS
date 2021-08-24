10aca42d800d261c7610c3822315a2f17c6c1cad
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-08-24 17:44:29 +0200

    Add axum-surf tests

--- ajars_test/tests/axum_reqwest_it.rs
+++ ajars_test/tests/axum_reqwest_it.rs
@@ -3,7 +3,7 @@ use ajars_test::axum::spawn_axum;
 use ajars_test::api::Simple;
 use std::time::Duration;
 
-use ajars::{Rest, RestFluent, RestType, actix_web::actix_web::http::header, reqwest::{reqwest::ClientBuilder, AjarsReqwest}};
+use ajars::{Rest, RestFluent, RestType, reqwest::{reqwest::ClientBuilder, AjarsReqwest}};
 
 #[actix_rt::test]
 async fn test_reqwest_rest() {
@@ -41,7 +41,6 @@ async fn perform_reqwest_call<REST: 'static + Clone + Send + RestType<Simple<Str
     // Act
     let response = ajars
         .request(rest)
-        .header(header::CONTENT_TYPE, "application/json")
         .send(&req_data).await;
 
     // Assert
