af1a86a6740965346cc67b2e3b694824d413b9f1
Francesco_Cina <mr.francesco.cina@gmail.com>
2021-12-08 17:42:22 +0100

    Add axum server example

---
+++ examples/server_axum/src/main.rs
@@ -0,0 +1,56 @@
+use ajars_common::{hello::*, ping::*};
+use ajars::axum::AxumHandler;
+use axum::{response::IntoResponse, http::{Uri, Method, Response}, body::{BoxBody, Body}, Router};
+use chrono::Local;
+use std::{fmt::Display, net::SocketAddr};
+
+/// The body type `PingRequest` and the result type `PingResult` are enforded at compile time
+async fn ping(body: PingRequest, uri: Uri, method: Method) -> Result<PingResponse, ServerError> {
+    println!("echo - Request path: {:?}", uri.path());
+    println!("echo - Request method: {:?}", method);
+    println!("echo - Request query_string: {:?}", uri.query());
+    println!("ping - Request body: {:?}", body);
+    Ok(PingResponse { message: format!("PONG - {}", Local::now()) })
+}
+
+/// The body type `HelloRequest` and the result type `HelloResponse` are enforded at compile time
+async fn hello(body: HelloRequest, uri: Uri, method: Method) -> Result<HelloResponse, ServerError> {
+    println!("echo - Request path: {:?}", uri.path());
+    println!("echo - Request method: {:?}", method);
+    println!("echo - Request query_string: {:?}", uri.query());
+    println!("hello - Request body: {:?}", body);
+    Ok(HelloResponse { hellos: body.names.iter().map(|name| format!("Hello, {}!", name)).collect() })
+}
+
+#[tokio::main]
+async fn main() {
+    let port = 8080;
+    println!("Starting actix server at port {}", port);
+
+    let app = Router::new()
+        .merge(PING.to(ping))
+        .merge(HELLO.to(hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    println!("Start axum to {}", addr);
+
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.expect("Axum server should start");
+
+}
+
+#[derive(Debug, Clone)]
+struct ServerError {}
+
+impl Display for ServerError {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
+
+impl IntoResponse for ServerError {
+
+    fn into_response(self) -> Response<BoxBody> {
+        Response::new(axum::body::boxed(Body::empty()))
+    }
+}
